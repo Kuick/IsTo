95482a0372a140951db3206870c581e0d80f2f3d
Chung, Chun-Yi <kevin@kuicker.org>
2016-03-04 10:38:32 +0800

    Support convert to or from struct type

---
+++ IsTo.Tests/Misc/Struct/TestStruct.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) kuicker.org. All rights reserved.
+// Modified By      YYYY-MM-DD
+// kevinjong        2016-03-04 - Creation
+
+using System;
+
+namespace IsTo.Tests
+{
+	public struct TestStruct
+	{
+		public int Property11;
+		public int Property12;
+		public int Property13;
+	}
+}
